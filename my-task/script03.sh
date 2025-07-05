f6a5c05d0fda3d9b3bbdf09a2980306df0da7ba9
jayalakshmikrishnan25@gmail.com <jayalakshmikrishnan25@gmail.com>
2025-07-05 22:51:57 +0530

    Created a folder and add few script files

---
+++ my-task/script03.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Script to check if a number is even or odd
+
+read -p "Enter a number: " num
+
+if [ $((num % 2)) -eq 0 ]; then
+    echo "$num is even."
+else
+    echo "$num is odd."
+fi
