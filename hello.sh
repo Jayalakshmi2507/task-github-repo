b0e5474b0becd01b575b420aa357f170e34ccd9f
jayalakshmikrishnan25@gmail.com <jayalakshmikrishnan25@gmail.com>
2025-09-25 13:02:41 +0530

    Updated hello.sh with a message hello hello

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Hello, Jaya! Your Jenkins build is successful."
 echo "Hello again, Jaya! This is your updated Jenkins build."
-echo"hello"
-echo"hello hello"
+echo "hello"
+echo "hello hello"
