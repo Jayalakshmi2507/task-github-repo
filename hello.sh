134429dcd49d944c323dbf294ba1c527d02ebfab
jayalakshmikrishnan25@gmail.com <jayalakshmikrishnan25@gmail.com>
2025-09-25 16:43:34 +0530

    Final build #12

--- hello.sh
+++ hello.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo "Hello, Jaya! Your Jenkins build is successful."
 echo "Hello again, Jaya! This is your updated Jenkins build."
-echo "hello"
-echo "hello hello"
-echo "Jenkins successful build"
-echo "Testing"
-echo "Testing again"
-echo "Final testing"
+echo "Successful jenkins build"
